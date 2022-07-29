4b3fbde92d3a8d4c596b83487b6a411b5ba8e40d
Rigyuki <304900272@qq.com>
2022-07-29 16:05:49 +0800

    update

---
+++ ProTest/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+public class BasicProcess
+{
+    public static string ProjectDir = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
+    public static void Main()
+    {
+        gitTest();      
+    }
+    
+    /// <summary>
+    /// 除了git拉取test之外的所有操作（都是需要传递参数的）在这里集中调用
+    /// </summary>
+    public static void testOperate()
+    {
+        GetILCatalogue("2020.3.33");
+        Local_il2cpp(@"E:\unity\2020.3.33f1c2\Editor\Data\il2cpp");
+        BuildTest(@"E:\unity\2020.3.33f1c2\Editor\Unity.exe", @"F:\hyclr\batTest\ProTest\ProTest\bin\Debug\net6.0\hybridclr_test");
+        ShowPlayerLog();
+    }
+    /// <summary>
+    /// 从git上拉取test项目
+    /// </summary>
+    /// <param name="gitBranch"></param>
+    public static void gitTest()
+    {       
+        string originDir = $"{ProjectDir}";        
+        Process p = new Process();
+        p.StartInfo.WorkingDirectory = originDir;
+        p.StartInfo.FileName = $"{originDir}/gitTest.bat";
+        p.StartInfo.UseShellExecute = false;
+        p.StartInfo.CreateNoWindow = false;
+        p.Start();
+        p.WaitForExit();
+        copyBash();
+    }
+
+    /// <summary>
+    /// 将bash文件复制到hybridclrdata文件夹
+    /// </summary>
+    public static void copyBash()
+    {
+        string originDir = $"{ProjectDir}/BasicBash";
+        string targetDir = $"{ProjectDir}/hybridclr_test/HybridCLRData";
+        Process p = new Process();
+        p.StartInfo.WorkingDirectory = originDir;
+        p.StartInfo.FileName = $"{originDir}/copyBash.bat";
+        p.StartInfo.Arguments = targetDir;
+        p.StartInfo.UseShellExecute = false;
+        p.StartInfo.CreateNoWindow = false;
+        p.Start();
+        p.WaitForExit();
+        testOperate();
+    }
+
+    /// <summary>
+    /// 从git上准备目录_repo和_plus目录
+    /// </summary>
+    /// <param name="gitBranch">获取git上的分支</param>
+    public static void GetILCatalogue(string gitBranch)
+    {
+        string targetDir = $"{ProjectDir}/hybridclr_test/HybridCLRData";
+        Process p = new Process();
+        p.StartInfo.WorkingDirectory = targetDir;
+        p.StartInfo.FileName = $"{targetDir}/GetILCatalogue.bat";
+        p.StartInfo.Arguments = gitBranch;
+        p.StartInfo.UseShellExecute = false;
+        p.StartInfo.CreateNoWindow = false;
+        p.Start();
+        p.WaitForExit();
+    }
+
+    /// <summary>
+    /// 当前的local_il2cpp路径
+    /// </summary>
+    /// <param name="localPath">2020.3.33的unity安装目录</param>
+    public static void Local_il2cpp(string localPath)
+    {
+        string targetDir = $"{ProjectDir}/hybridclr_test/HybridCLRData";
+        Process p = new Process();
+        p.StartInfo.WorkingDirectory = targetDir;
+        p.StartInfo.FileName = $"{targetDir}/Libil2cpp.bat";
+        p.StartInfo.Arguments = localPath;
+        p.StartInfo.UseShellExecute = false;
+        p.StartInfo.CreateNoWindow = false;
+        p.Start();
+        p.WaitForExit();
+    }
+
+    /// <summary>
+    /// 打包win
+    /// </summary>
+    /// <param name="unityPath">unty路径</param>
+    /// <param name="projectPath">project路径</param>
+    public static void BuildTest(string unityPath,string projectPath)
+    {
+        string targetDir = $"{ProjectDir}/hybridclr_test/HybridCLRData";
+        Process p = new Process();
+        p.StartInfo.WorkingDirectory = targetDir;
+        p.StartInfo.FileName = $"{targetDir}/BuildWin.bat";
+        p.StartInfo.Arguments = string.Format("{0} {1}", unityPath, projectPath);
+        p.StartInfo.UseShellExecute = false;
+        p.StartInfo.CreateNoWindow = false;
+        p.Start();
+        p.WaitForExit();
+    }
+
+    /// <summary>
+    /// 输出两个日志
+    /// </summary>
+    public static void ShowPlayerLog()
+    {
+        string targetDir = $"{ProjectDir}/hybridclr_test/HybridCLRData";
+        Process p = new Process();
+        p.StartInfo.WorkingDirectory = targetDir;
+        p.StartInfo.FileName = $"{targetDir}/ShowPlayerLog.bat";
+        p.StartInfo.UseShellExecute = false;
+        p.StartInfo.CreateNoWindow = false;
+        p.Start();
+        p.WaitForExit();
+    }
+}
+//DirectoryInfo di = new DirectoryInfo(Directory.GetCurrentDirectory());
+// public static string ProjectDir => Directory.GetCurrentDirectory().ToString();
+//public static string ProjectDir => Directory.GetParent(Application.dataPath).ToString();
+/*        string targetDir = $"{ProjectDir}/Bash";
+        Process p = new Process();
+        p.StartInfo.WorkingDirectory = targetDir;
+        p.StartInfo.FileName = "Libil2cpp.bat";
+        *//*ProcessStartInfo pi = new ProcessStartInfo(p.StartInfo.FileName, parm);//第二个参数为传入的参数，string类型以空格分隔各个参数  
+        pi.UseShellExecute = false;
+        pi.RedirectStandardOutput = true;*//*
+        p.StartInfo.Arguments = parm;
+        p.StartInfo.UseShellExecute = false;
+        p.StartInfo.RedirectStandardOutput = true;
+
+        //pi.FileName= "buildIL.bat";
+        p.StartInfo.UseShellExecute = false;
+        p.StartInfo.CreateNoWindow = true;
+        //p.StartInfo = pi;
+        p.Start();
+        p.WaitForExit();
+        string output = p.StandardOutput.ReadToEnd();*/
+
+/* string targetDir = $"{ProjectDir}/BasicBash";
+ Process p = new Process();
+ *//*p.StartInfo.WorkingDirectory = targetDir;
+ p.StartInfo.FileName = $"{targetDir}/BuildAndroid.bat";*//*
+
+ ProcessStartInfo pi = new ProcessStartInfo()
+ {
+     ArgumentList = {
+         unityPath,
+         projectPath,
+     }
+ };
+ pi.UseShellExecute = false;
+ pi.RedirectStandardOutput = true;
+ pi.FileName= $"{targetDir}/BuildWin.bat";
+ pi.WorkingDirectory = targetDir;
+ p.StartInfo = pi;   
+ p.Start();
+ p.WaitForExit();*/
+/*p.StartInfo.Arguments = parm1;
+ p.StartInfo.UseShellExecute = false;
+p.StartInfo.CreateNoWindow = false;
+p.Start();
+p.WaitForExit();*/
+//string output = p.StandardOutput.ReadToEnd();
+// GetGit("2020.3.33");
+// Local_il2cpp(@"E:\unity\2020.3.33f1c2\Editor\Data\il2cpp");
+// BuildTest(@"E:\unity\2020.3.33f1c2\Editor\Unity.exe", @"F:\hyclr\batTest\hybridclr_test");
